457ec9a9d2078811701f27255f5d2d7c52f59289
pinocchio22 <vviian.2@gmail.com>
2021-09-17 17:28:30 +0900

    feat : NaverAPI
    Retrofit과 OkHttp를 활용하여 API를 초기화 시키고 참조할 인터페이스를 만듬

---
+++ app/src/main/java/com/example/corona_ujb/NaverAPI.kt
@@ -0,0 +1,52 @@
+package com.example.corona_ujb
+
+import java.util.*
+
+/**
+ * @author CHOI
+ * @email vviian.2@gmail.com
+ * @created 2021-09-17
+ * @desc
+ */
+interface NaverAPI {
+
+    @GET("v1/search/news.json")
+    fun getSearchNews(
+        @Query("query") query: String,
+        @Query("display") display: Int? = null,
+        @Query("start") start: Int? = null
+    ): Observable<ResultGetSearchNews>
+
+    companion object {
+        private const val BASE_URL_NAVER_API = "https://openapi.naver.com"
+        private const val CLIENT_ID = "아이디"
+        private const val CLIENT_SECRET = "비밀번호"
+
+        fun create(): NaverAPI {
+            val httpLoggingInterceptor = HttpLoggingInterceptor()
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            val headerInterceptor = Interceptor {
+                val request = it.request()
+                    .newBuilder()
+                    .addHeader("X-Naver-Client-Id", CLIENT_ID)
+                    .addHeader("X-Naver-Client-Secret", CLIENT_SECRET)
+                    .build()
+                return@Interceptor it.proceed(request)
+            }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(headerInterceptor)
+                .addInterceptor(httpLoggingInterceptor)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL_NAVER_API)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+                .create(NaverAPI::class.java)
+        }
+    }
+}
