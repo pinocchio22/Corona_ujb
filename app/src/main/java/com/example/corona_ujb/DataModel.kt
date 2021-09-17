96760abfad91bcc27e7631275009b0915dcebf7f
pinocchio22 <vviian.2@gmail.com>
2021-09-17 18:09:26 +0900

    feat : MaskAPI

---
+++ app/src/main/java/com/example/corona_ujb/DataModel.kt
@@ -0,0 +1,53 @@
+package com.example.corona_ujb
+
+/**
+ * @author CHOI
+ * @email vviian.2@gmail.com
+ * @created 2021-09-17
+ * @desc
+ */
+
+data class ResultGetSearchNews(
+
+    var start: Int = 0,
+    var display: Int = 0,
+    var items: List<NewsItems>
+)
+
+data class NewsItems(
+    var title: String = "",
+    var link: String = "",
+    var description: String = "",
+    var pubDate: String = ""
+)
+
+data class PharmacyItems(
+    var title: String = "",
+    var pnum: String = "",
+    var link: String = ""
+)
+
+data class CoronaPeople(
+    var name: String = "",
+    var route: String = "",
+    var num: String = "",
+    var hospital: String = "",
+    var date: String = ""
+)
+
+data class ResultGetMaskData(
+    var count: Int = 0,
+    var stores: List<MaskLatlon>
+)
+
+data class MaskLatlon(
+    var code: String = "",
+    var created_at: String = "",
+    var remain_stat: String = "",
+    var stock_at: String = "",
+    var addr: String = "",
+    var lat: Double = 0.0,
+    var lng: Double= 0.0,
+    var name: String = "",
+    var type: String = ""
+)
---
+++ app/src/main/java/com/example/corona_ujb/MaskAPI.kt
@@ -0,0 +1,55 @@
+package com.example.corona_ujb
+
+import io.reactivex.Observable
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * @author CHOI
+ * @email vviian.2@gmail.com
+ * @created 2021-09-17
+ * @desc
+ */
+interface MaskAPI {
+    @GET("storesByGeo/json")
+    fun getMaskLatlng(
+        @Query("lat") lat: Double,
+        @Query("lng") display: Double,
+        @Query("m") start: Int
+    ): Observable<ResultGetMaskData>
+
+    companion object {
+        private const val BASE_URL_MASK_API = "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/"
+
+        fun create(): MaskAPI {
+            val httpLoggingInterceptor = HttpLoggingInterceptor()
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            val headerInterceptor = Interceptor {
+                val request = it.request()
+                    .newBuilder()
+                    .build()
+                return@Interceptor it.proceed(request)
+            }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(headerInterceptor)
+                .addInterceptor(httpLoggingInterceptor)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL_MASK_API)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+                .create(MaskAPI::class.java)
+        }
+    }
+}
