c2d018bb82182baa0bd2759d42dfa6343b628433
pinocchio22 <vviian.2@gmail.com>
2021-09-18 20:23:21 +0900

    feat : MainActivity.kt

--- app/src/main/java/com/example/corona_ujb/MainActivity.kt
+++ app/src/main/java/com/example/corona_ujb/MainActivity.kt
@@ -1,9 +1,31 @@
 package com.example.corona_ujb
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var FINISH_INTERVAL_TIME: Long = 1500
+    private var backPressedTime: Long = 0
+    var REQUIRED_PERMISSIONS = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+    val num=1
+    val num2 =2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -11,5 +33,112 @@ class MainActivity : AppCompatActivity() {
         val vpAdapter: VPAdapter =
             VPAdapter(supportFragmentManager)
         viewpager.adapter = vpAdapter
+        tab.setupWithViewPager(viewpager)
+    }
+
+    override fun onBackPressed() {
+        val tempTime = System.currentTimeMillis()
+        val intervalTime = tempTime - backPressedTime
+        if (!(0 > intervalTime || FINISH_INTERVAL_TIME < intervalTime)) {
+            finishAffinity()
+            System.runFinalization()
+            System.exit(0)
+        } else {
+            backPressedTime = tempTime
+            Toast.makeText(this, "'뒤로' 버튼을 한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        super.onBackPressed()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!checkLocationServicesStatus()) {
+            showDialogForLocationServiceSetting()
+        } else {
+            checkRunTimePermission()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+            permsRequestCode: Int,
+            permissions: Array<String>,
+            grandResults: IntArray
+    ) {
+        if (permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.size == REQUIRED_PERMISSIONS.size) { // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
+            var check_result = true
+            // 모든 퍼미션을 허용했는지 체크합니다.
+            for (result in grandResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false
+                    break
+                }
+            }
+            if (check_result) { //위치 값을 가져올 수 있음
+            } else { // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])
+                        || ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[1])) {
+                    Toast.makeText(this, "권한이 거부되었습니다. 앱을 다시 실행하여 위치 권한을 허용해주세요.", Toast.LENGTH_LONG).show()
+                    finish()
+                } else {
+                    Toast.makeText(this, "권한이 거부되었습니다. 설정(앱 정보)에서 위치 권한을 허용해야 합니다. ", Toast.LENGTH_LONG).show()
+                    finish()
+                }
+            }
+        }
+    }
+
+    fun checkRunTimePermission() { //런타임 퍼미션 처리
+// 위치 퍼미션 체크
+        val hasFineLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED && hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])) {
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE) }
+            else { ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE)
+            }
+        }
+    }
+
+    // GPS 활성화를 위한 메소드들
+    fun showDialogForLocationServiceSetting() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("위치 서비스 비활성화")
+        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n" + "위치 설정을 수정하시겠습니까?")
+        builder.setPositiveButton("설정") { dialog, id ->
+            val callGPSSettingIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE)
+        }
+        builder.setNegativeButton("취소") { dialog, id ->
+            dialog.cancel()}
+        builder.setCancelable(false)
+        builder.create().show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            GPS_ENABLE_REQUEST_CODE ->  //사용자가 GPS 활성 시켰는지 검사
+                if (checkLocationServicesStatus()) {
+                    if (checkLocationServicesStatus()) {
+                        Log.d("@@@", "onActivityResult : GPS 활성화 되있음")
+                        checkRunTimePermission()
+                        return
+                    }
+                }
+        }
+    }
+
+    fun checkLocationServicesStatus(): Boolean {
+        val locationManager =
+                getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+    }
+
+    companion object {
+        private const val GPS_ENABLE_REQUEST_CODE = 2001
+        private const val PERMISSIONS_REQUEST_CODE = 100
     }
 }
