26baa4ad0518a7acf4250a608a4ef2b10002383a
pinocchio22 <vviian.2@gmail.com>
2021-09-29 20:44:20 +0900

    feat : DialogViewModel
    added DialogViewModel.kt

---
+++ app/src/main/java/com/example/corona_ujb/DialogViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.corona_ujb
+
+import android.annotation.SuppressLint
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class DialogViewModel(): ViewModel(){
+    private val repo : CoronaRepository =
+        CoronaRepository()
+    var pharmacyList: MutableLiveData<ArrayList<PharmacyItems>> = MutableLiveData<ArrayList<PharmacyItems>>()
+    private var pharmacyAdapter =
+        PharmacyAdapter(this)
+    var click: MutableLiveData<Int> = MutableLiveData<Int>(-1)
+    var clickMap: MutableLiveData<Int> = MutableLiveData<Int>(-1)
+
+    init {
+        getPharmacy()
+    }
+    @SuppressLint("CheckResult")
+    fun getPharmacy() {
+        repo.getPharmacy().subscribe {
+                list -> pharmacyList.value = list
+        }
+    }
+
+    fun init(recyclerView: RecyclerView, adapter: PharmacyAdapter) {
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(recyclerView.context)
+    }
+
+    fun getAdapter(): PharmacyAdapter = pharmacyAdapter
+}
