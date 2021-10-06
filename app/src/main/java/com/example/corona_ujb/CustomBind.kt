fdfe7b6d6e6657ad386b647f0563751b61215809
pinocchio22 <vviian.2@gmail.com>
2021-10-06 21:17:55 +0900

    bug
    Didn't find class "com.example.corona_ujb.FirebaseMessagingService" on path: DexPathList

---
+++ app/src/main/java/com/example/corona_ujb/CustomBind.kt
@@ -0,0 +1,16 @@
+package com.example.corona_ujb
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+object CustomBind {
+
+        @JvmStatic
+        @BindingAdapter("bind:verAdapter")
+        fun searchAdapter(recyclerView: RecyclerView, adapter: RecyclerView.Adapter<*>?) {
+            recyclerView.adapter = adapter
+            recyclerView.layoutManager = LinearLayoutManager(recyclerView.context)
+        }
+
+}
---
+++ app/src/main/java/com/example/corona_ujb/SplashActivity.kt
@@ -0,0 +1,88 @@
+package com.example.corona_ujb
+
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.messaging.FirebaseMessaging
+
+/**
+ * @author CHOI
+ * @email vviian.2@gmail.com
+ * @created 2021-09-17
+ * @desc
+ */
+
+class SplashActivity : AppCompatActivity() {
+
+    @SuppressLint("StringFormatInvalid")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if(SharePreferences.prefs.getAlert("splash", 0) == 0) {
+            FirebaseMessaging.getInstance().subscribeToTopic("alert")
+                .addOnCompleteListener { task -> }
+            SharePreferences.prefs.setAlert("splash", 1)
+        }
+        Thread.sleep(500)
+        NetworkCheck()
+    }
+
+    fun NetworkIsValid(context: Context): Boolean {
+        var result = false
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities = cm.activeNetwork ?: return false
+            val actNw = cm.getNetworkCapabilities(networkCapabilities) ?: return false
+            result = when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            cm.run {
+                cm.activeNetworkInfo?.run {
+                    result = when (type) {
+                        ConnectivityManager.TYPE_WIFI -> true
+                        ConnectivityManager.TYPE_MOBILE -> true
+                        ConnectivityManager.TYPE_ETHERNET -> true
+                        else -> false
+                    }
+                }
+            }
+        }
+        return result
+    }
+
+    fun NetworkCheck() {
+        if (NetworkIsValid(this)) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            val dlg: AlertDialog.Builder = AlertDialog.Builder(this)
+            dlg.setTitle("알림") //제목
+            dlg.setMessage("본 앱은 인터넷 연결이 필요합니다.\n네트워크 상태를 확인해주세요.") // 메시지
+            dlg.setCancelable(false)
+            dlg.setPositiveButton("재시도", DialogInterface.OnClickListener { dialog, which ->
+                if (NetworkIsValid(this)) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    NetworkCheck()
+                }
+            })
+            dlg.setNegativeButton("종료", DialogInterface.OnClickListener { dialog, which ->
+                finish()
+            })
+            dlg.show()
+        }
+    }
+}
